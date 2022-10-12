6da1b87f50425645d1409ebc3ef84456bf3f6484
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-12 10:22:14 +0500

    07

--- if07.py
+++ if07.py
@@ -12,20 +12,19 @@ def main(a):
     Returns:
         string: the message to print
     """
-    x=0
     if a>0:
-        a%2==1
-        x="positive odd number"
+        if a%2==1:
+            x="positive odd number"
     if a>0:
-        a%2==0
-        x="positive even number"
+        if a%2==0:
+            x="positive even number"
     if a<0:
-        a%2==1
-        x="negative odd number"
+        if a%2==1:
+            x="negative odd number"
     if a<0:
-        a%2==0  
-        x="negative even number"
+       if a%2==0:
+            x="negative even number"
     if a==0:       
         x="the number is zero"
     return x
-print(main(0))
+print(main(-9))
