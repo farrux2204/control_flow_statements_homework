2480022e66abfa7a4a6e981d1e3d9fa7329dfe7c
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-12 09:41:07 +0500

    07

--- if07.py
+++ if07.py
@@ -17,13 +17,14 @@ def main(a):
         x="positive odd number"
     if a>0:
         a%2==0
-        y="positive even number"
+        x="positive even number"
     if a<0:
         a%2==1
-        z="negative odd number"
-    if a<0
+        x="negative odd number"
+    if a<0:
         a%2==0
-        f="negative even number"
-    if a==zero:
-        v="the number is zero"
-    return v,f,x,y,z
+        x="negative even number"
+    if a==0:
+        x="the number is zero"
+    return x
+print(main(-5))
